6cc706b8f954baf841ffb1dd072143dca440f686
j3lmer <jelmer339@gmail.com>
2022-08-31 12:39:51 +0200

    created misc files

---
+++ countNothing.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int c;
+
+	int blanks, tabs, newlines = 0;
+		
+	
+	while ((c = getchar()) != EOF)  {
+
+		switch (c) {
+			case ' ':
+				blanks++;
+				break;
+				
+			case '\t':
+				tabs++;
+				break;
+
+			case '\n':
+				newlines++;
+				break;
+		}
+	}
+
+	printf("blanks: %d\ntabs: %d\n newlines: %d\n", blanks, tabs, newlines);
+		
+}
